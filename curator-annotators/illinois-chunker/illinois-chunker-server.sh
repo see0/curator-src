c40cf3d0872bb04ec0afb96a3f3ff37d3555bdf3
clarkeje <clarkeje@e05960e4-3e65-0410-b41c-c37a2a91760e>
2010-08-17 16:39:43 +0000

    upgrade to commons-lang 2.5
    git-svn-id: https://subversion.cs.uiuc.edu/svn/cogcomp/trunk/curator@10510 e05960e4-3e65-0410-b41c-c37a2a91760e

--- curator-annotators/illinois-chunker/illinois-chunker-server.sh
+++ curator-annotators/illinois-chunker/illinois-chunker-server.sh
@@ -9,7 +9,7 @@ COMPONENTDIR=$CURATOR_BASE/components
 
 COMPONENT_CLASSPATH=$COMPONENTDIR/illinois-chunker-server.jar:$COMPONENTDIR/curator-interfaces.jar
 
-LIB_CLASSPATH=$LIBDIR/LBJ2Library.jar:$LIBDIR/LBJPOS.jar:$LIBDIR/LBJChunk.jar:$LIBDIR/commons-cli-1.2.jar:/commons-lang-2.4.jar:$LIBDIR/commons-logging-1.1.1.jar::$LIBDIR/h2-1.1.118.jar:$LIBDIR/libthrift.jar:$LIBDIR/logback-classic-0.9.17.jar:$LIBDIR/logback-core-0.9.17.jar:$LIBDIR/slf4j-api-1.5.8.jar
+LIB_CLASSPATH=$LIBDIR/LBJ2Library.jar:$LIBDIR/LBJPOS.jar:$LIBDIR/LBJChunk.jar:$LIBDIR/commons-cli-1.2.jar:/commons-lang-2.5.jar:$LIBDIR/commons-logging-1.1.1.jar::$LIBDIR/h2-1.1.118.jar:$LIBDIR/libthrift.jar:$LIBDIR/logback-classic-0.9.17.jar:$LIBDIR/logback-core-0.9.17.jar:$LIBDIR/slf4j-api-1.5.8.jar
 
 CLASSPATH=$COMPONENT_CLASSPATH:$LIB_CLASSPATH
 
--- curator-annotators/illinois-ner/illinois-ner-server.sh
+++ curator-annotators/illinois-ner/illinois-ner-server.sh
@@ -11,7 +11,7 @@ COMPONENT_CLASSPATH=$COMPONENTDIR/illinois-ner-server.jar:$COMPONENTDIR/curator-
 
 # ner has a dependency on an older LBJ2Library (2.2.2)
 
-LIB_CLASSPATH=$LIBDIR/LbjNerTagger.jar:$LIBDIR/LBJ2Library-2.2.2.jar:$LIBDIR/commons-cli-1.2.jar:/commons-lang-2.4.jar:$LIBDIR/commons-logging-1.1.1.jar::$LIBDIR/h2-1.1.118.jar:$LIBDIR/libthrift.jar:$LIBDIR/logback-classic-0.9.17.jar:$LIBDIR/logback-core-0.9.17.jar:$LIBDIR/slf4j-api-1.5.8.jar
+LIB_CLASSPATH=$LIBDIR/LbjNerTagger.jar:$LIBDIR/LBJ2Library-2.2.2.jar:$LIBDIR/commons-cli-1.2.jar:/commons-lang-2.5.jar:$LIBDIR/commons-logging-1.1.1.jar::$LIBDIR/h2-1.1.118.jar:$LIBDIR/libthrift.jar:$LIBDIR/logback-classic-0.9.17.jar:$LIBDIR/logback-core-0.9.17.jar:$LIBDIR/slf4j-api-1.5.8.jar
 
 CLASSPATH=$COMPONENT_CLASSPATH:$LIB_CLASSPATH:$NER_CLASSPATH
 
